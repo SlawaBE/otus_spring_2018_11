6603813af5c4ea9eb5b61bff8c9c59c80d1b97cf
SlawaBE <britvin.slawa@yandex.ru>
2018-12-22 23:44:52 +0300

    fix test

--- src/test/java/ru/otus/spring01/MainSpringBootTest.java
+++ src/test/java/ru/otus/spring01/MainSpringBootTest.java
@@ -5,27 +5,21 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.test.context.junit4.SpringRunner;
-import ru.otus.spring01.domain.Result;
-import ru.otus.spring01.service.QuestionService;
-import ru.otus.spring01.service.ResultCheckService;
-import ru.otus.spring01.service.TestingService;
-import ru.otus.spring01.service.TestingServiceImpl;
+import ru.otus.spring01.dao.QuestionDao;
+import ru.otus.spring01.dao.QuestionDaoImpl;
+import ru.otus.spring01.service.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.util.List;
 import java.util.Locale;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.Mockito.*;
 
 /**
  * Created by Stanislav on 16.12.2018
@@ -45,15 +39,24 @@ public class MainSpringBootTest {
             return ms;
         }
 
-        @MockBean
-        public static QuestionService questionService;
-
-        @MockBean
-        public static ResultCheckService resultCheckService;
+        @Bean
+        public QuestionDao questionDao() {
+            return new QuestionDaoImpl(messageSource());
+        }
 
         @Bean
-        public TestingService getTestingService() {
-            return new TestingServiceImpl(questionService, resultCheckService, messageSource());
+        public QuestionService getQuestionService() {
+            return new QuestionServiceImpl(questionDao());
+        }
+
+        @Bean
+        public ResultCheckService resultCheckService() {
+            return new ResultCheckServiceImpl();
+        }
+
+        @Bean
+        public TestingService testingService() {
+            return new TestingServiceImpl(getQuestionService(), resultCheckService(), messageSource());
         }
     }
 
@@ -62,18 +65,40 @@ public class MainSpringBootTest {
 
     @Before
     public void init() {
-        when(Config.resultCheckService.checkResult(any(List.class))).thenReturn(new Result(1, 1));
+        Locale.setDefault(new Locale("ru_RU"));
     }
 
     @Test
     public void test() {
-        ByteArrayInputStream bais = new ByteArrayInputStream("test\ntest\n2\n1\n2\n3\n2\n".getBytes());
+        ByteArrayInputStream bais = new ByteArrayInputStream("test\ntest\n2\n2\n".getBytes());
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        String expected = "Введите ваше имя: Введите вашу фамилию: question1\n" +
+                "0) answer1\n" +
+                "1) answer2\n" +
+                "2) answer3\n" +
+                "\n" +
+                "Укажите номер выбранного вами ответа: question2\n" +
+                "0) answer1\n" +
+                "1) answer2\n" +
+                "\n" +
+                "Укажите номер выбранного вами ответа: \n" +
+                "---------------------------------------\n" +
+                "Результаты теста\n" +
+                "Студент: test test\n" +
+                "---------------------------------------\n" +
+                "Правильных ответов: 1\n" +
+                "Всего вопросов:     2\n" +
+                "---------------------------------------\n" +
+                "Итоговая оценка:    1/2\n" +
+                "---------------------------------------\n";
         testingService.test(bais, baos);
-        verify(Config.questionService).getQuestions();
-        verify(Config.resultCheckService).checkResult(anyList());
         assertTrue(baos.size() > 0);
-        System.out.println(baos.toString());
+        assertEquals(expected, prepareString(baos));
+    }
+
+    private String prepareString(ByteArrayOutputStream os) {
+        String str = os.toString();
+        return str.replaceAll("\r", "");
     }
 
 }
